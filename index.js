b5fdcf10983ec1857ce2c3980b555af82b2c222c
JABermudez11 <justinanbermudez@gmail.com>
2020-03-18 16:07:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+
+function findMatching(drivers, name) {
+    let list = drivers.filter(n => {
+        return (n.toLowerCase() == name.toLowerCase())
+    })
+    return list;
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter( letters =>
+        letters.toLowerCase().indexOf(name.toLowerCase()) === 0
+    )
+}
+
+function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+}
